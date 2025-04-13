3a272b86c25dda0767a7ebd9f60087baa707be9f
saranprithvick <prithvicksaran11@gmail.com>
2025-04-13 16:19:05 +0530

    updated models

--- bidbuygo/models.py
+++ bidbuygo/models.py
@@ -22,5 +22,38 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
     
+class Orders(models.Model):
+    order_id = models.CharField(max_length=25,primary_key=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    order_address = models.TextField()
+    price_amt = models.DecimalField(max_digits=10,decimal_places=2)
+    order_date = models.DateTimeField()
+    order_status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Order {self.order_id}"
+    
+class Delivery(models.Model):
+    delivery_id = models.CharField(max_length=25,primary_key=True)
+    order = models.ForeignKey(Orders,on_delete=models.CASCADE)
+    courier_service = models.CharField(max_length=50)
+    tracking_id = models.CharField(max_length=25,unique=True)
+    delivery_status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Delivery {self.delivery_id}"
+    
+class Tracking(models.Model):
+    tracking_id = models.CharField(max_length=25, primary_key=True)
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tracking_id
+    
+
+    
+
+    
 
 
