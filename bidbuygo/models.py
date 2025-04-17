dec1531fe50192094f39ca628b74ff7abf9dee11
saranprithvick <prithvicksaran11@gmail.com>
2025-04-17 17:32:25 +0530

    Enhance home page layout and update settings for static files

--- bidbuygo/models.py
+++ bidbuygo/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=2)
     quantity = models.IntegerField(null=False)
     review = models.TextField(blank=True,null=True)
+    #image = models.ImageField(upload_to='products/', blank=True, null=True)
 
     def __str__(self):
         return self.product_name
